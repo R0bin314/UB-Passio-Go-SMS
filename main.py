be2bdc152f7944007c0477828a572657d5db6912
robing <robingallagher.inbox@gmail.com>
2025-09-03 17:49:52 -0400

    Fixed key error

--- main.py
+++ main.py
@@ -18,40 +18,58 @@ def print_stops():
         print("")
         i+=1
 
-
-
 def select_route():
-    route_selection = input("\nSelect a route: ")
-    print("")
+    i=0
+    for route in UB_routes:
+        print(str(i) + " " + route.name)
+        i+=1
 
+    route_selection = input("\nSelect a route: ")
+    route = UB_routes[int(route_selection)]
+    print("")
+    i = 0
     if int(route_selection) <= len(UB_routes) or int(route_selection) < 0:
         print("Here is your route information...")
-        print("Route name: "+ UB_routes[0].name)
+        print("Route name: "+ route.name)
         print("Stops:")
-        for stop in UB_routes[0].getStops():
-            print(stop.name)
+        for stop in route.getStops():
+            print(str(i) + " " + stop.name)
+            i+=1
+        return [route_selection, route]
     else:
         print("Invalid route selected.")
+        return None
+
+
+def select_stop():
+    route_info = select_route()
+    route_selection = route_info[0]
+    route = route_info[1]
+
+    stop_selection = input("\nSelect a stop: ")
+    get_stop_info(route_selection, stop_selection)
+
+
+def get_stop_info(route_id, stop_id):
+    route = UB_routes[int(route_id)]
+
+    stop = route.getStops()[int(stop_id)] # OK up to here
+    print("Route Name: " + route.name + " (ID " + str(stop_id) + ")")
+    print("Stop Name: " + stop.name)
+    get_etas(stop.id)
 
 def get_etas(stop_id):
     #Need to ensure stop_id is string
     stop_data = requests.get("https://passiogo.com/mapGetData.php?eta=3&deviceId=" +
                              str(random.randint(10000000,99999999)) +"&stopIds=" + str(stop_id))
     stop_json = stop_data.json()
+
     eta_list = stop_json['ETAs'][stop_id] # Lists all ETAs for stop w/ info about buses, etc.
 
     for eta in eta_list:
         print("* " + eta['eta'])
 
-def get_stop_info(stop_id):
-    route = UB_routes[int(stop_id)]
-    stop = UB_routes[stop_id].getStops()[0]
-    stop_id = stop.id
-    print("Route Name: " + route.name + " (ID " + str(stop_id) + ")")
-    print("Stop Name: " + stop.name)
-    get_etas(stop_id)
-
-get_stop_info(0)
+select_stop()
 
 """
 all_stops_id = 3
