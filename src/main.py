4770cf1ea0464373782d87e4710c8080bd7bc454
robing <robingallagher.inbox@gmail.com>
2025-09-03 19:25:28 -0400

    Refactored code

--- src/main.py
+++ src/main.py
@@ -1,67 +1,64 @@
-import random
-from xml.etree.ElementTree import tostring
+"""
+TODO:
+Split main and terminal_menu code
+Maybe turn this into a module so I can create SMS and terminal?
+"""
 
+import random
 import passiogo
 import requests
-import json
 UB_STAMPEDE_ID = 4882 # Stampede
 UB_SHUTTLE_ID = 5230 # Shuttle
+
+BASE_URL = "https://passiogo.com/mapGetData.php?eta=3&deviceId="
 UB_system = passiogo.getSystemFromID(UB_STAMPEDE_ID)
 UB_routes = UB_system.getRoutes()
 
-def print_stops():
-    i = 0
-    for route in UB_routes:
-        print("Route " + str(i) + ": " + route.name)
-        for stop in route.getStops():
-            print(stop.name)
-        print("")
-        i+=1
-
 def select_route():
+    print("Please select a route:")
+
     i=0
     for route in UB_routes:
-        print(str(i) + " " + route.name)
+        print(str(i) + ". " + route.name)
         i+=1
 
-    route_selection = input("\nSelect a route: ")
+    route_selection = input("\nRoute selection: ")
     route = UB_routes[int(route_selection)]
-    print("")
-    i = 0
+
     if int(route_selection) <= len(UB_routes) or int(route_selection) < 0:
-        print("Here is your route information...")
-        print("Route name: "+ route.name)
-        print("Stops:")
-        for stop in route.getStops():
-            print(str(i) + " " + stop.name)
-            i+=1
-        return [route_selection, route]
+        print_route_info(route)
+        return route
     else:
         print("Invalid route selected.")
         return None
 
+def print_route_info(route):
+    i = 0
+    print("\nHere is your route information...")
+    print("Route name: " + route.name)
+    print("Stops:")
+    for stop in route.getStops():
+        print(str(i) + " " + stop.name)
+        i += 1
 
 def select_stop():
-    route_info = select_route()
-    route_selection = route_info[0]
-    route = route_info[1]
+    route = select_route()
 
     stop_selection = input("\nSelect a stop: ")
-    get_stop_info(route_selection, stop_selection)
+    stop = route.getStops()[int(stop_selection)]
 
+    print_stop_info(route, stop)
 
-def get_stop_info(route_id, stop_id):
-    route = UB_routes[int(route_id)]
-
-    stop = route.getStops()[int(stop_id)] # OK up to here
-    print("Route Name: " + route.name + " (ID " + str(stop_id) + ")")
+def print_stop_info(route, stop):
+    print("Route Name: " + route.name)
     print("Stop Name: " + stop.name)
-    get_etas(stop.id)
+    get_etas(stop)
 
-def get_etas(stop_id):
+def get_etas(stop):
     #Need to ensure stop_id is string
-    stop_data = requests.get("https://passiogo.com/mapGetData.php?eta=3&deviceId=" +
-                             str(random.randint(10000000,99999999)) +"&stopIds=" + str(stop_id))
+    stop_id = stop.id
+    stop_data = requests.get(BASE_URL +
+                             str(random.randint(10000000,99999999)) + "&stopIds=" + str(stop_id))
     stop_json = stop_data.json()
 
     eta_list = stop_json['ETAs'][stop_id] # Lists all ETAs for stop w/ info about buses, etc.
@@ -70,11 +67,3 @@ def get_etas(stop_id):
         print("* " + eta['eta'])
 
 select_stop()
-
-"""
-To find ID:
-systems = passiogo.getSystems()
-
-for system in systems:
-    print(str(system.id)+ " " + system.name)
-"""
